1a5278ed7f5996d81f380df721d27634d0952f99
Вовк Ольга <vovkolga@macbook-vovk.local>
2019-02-08 16:34:43 +0200

    Initial Commit

---
+++ Taskly/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Taskly
+//
+//  Created by Вовк Ольга' on 2/8/19.
+//  Copyright © 2019 Вовк Ольга'. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
